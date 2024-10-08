861aeaca94fdf7b459569d46af944a9b67e89dc6
xamfoo <xamfoo@icloud.com>
2024-10-08 15:44:31 +0800

    Fix better-escape config for latest version

--- init.lua
+++ init.lua
@@ -369,7 +369,33 @@ require('lazy').setup({
     'max397574/better-escape.nvim',
     config = function()
       require('better_escape').setup {
-        mapping = { 'jk' },
+        timeout = vim.o.timeoutlen,
+        default_mappings = false,
+        mappings = {
+            i = {
+                j = {
+                    -- These can all also be functions
+                    k = '<Esc>',
+                    j = '<Esc>',
+                },
+            },
+            c = {
+                j = {
+                    k = '<Esc>',
+                    j = '<Esc>',
+                },
+            },
+            v = {
+                j = {
+                    k = '<Esc>',
+                },
+            },
+            s = {
+                j = {
+                    k = '<Esc>',
+                },
+            },
+        },
       }
     end,
   },
