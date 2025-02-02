35e90a833b4cbd0719179ca57aa6fc54d6797601
xamfoo <xamfoo@icloud.com>
2025-02-02 20:59:26 +0800

    LSP config for nix

--- init.lua
+++ init.lua
@@ -731,6 +731,7 @@ require('lazy').setup({
             },
           },
         },
+        nixd = {},
         pylsp = {},
         terraformls = {},
         yamlls = {},
@@ -752,7 +753,7 @@ require('lazy').setup({
 
       ---@diagnostic disable-next-line: missing-fields
       require('nvim-treesitter.configs').setup {
-        ensure_installed = { 'bash', 'c', 'html', 'lua', 'markdown', 'vim', 'vimdoc' },
+        ensure_installed = { 'bash', 'c', 'html', 'lua', 'markdown', 'nix', 'vim', 'vimdoc' },
         -- Autoinstall languages that are not installed
         auto_install = true,
         highlight = { enable = true },
