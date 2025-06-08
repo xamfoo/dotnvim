3a19bb3e00003359d2e2e39648d2aa28be8a5e93
xamfoo <xamfoo@icloud.com>
2025-06-08 10:26:21 +0800

    Remove unused option

--- init.lua
+++ init.lua
@@ -375,7 +375,6 @@ require('lazy').setup({
   },
   {
     'kylechui/nvim-surround',
-    version = '*', -- Use for stability; omit to use `main` branch for the latest features
     event = 'VeryLazy',
     opts = {},
   },
