ed87ffe3a793e24ac3dcbf85132d984c1f86fd99
xamfoo <xamfoo@icloud.com>
2026-08-19 22:28:00 +0800

    Configure vtsls lsp

--- init.lua
+++ init.lua
@@ -1203,11 +1203,23 @@ require('lazy').setup({
         -- pyright = {},
         -- rust_analyzer = {},
         --
-        -- Some languages (like typescript) have entire language plugins that can be useful:
-        --    https://github.com/pmizio/typescript-tools.nvim
-        --
-        -- But for many setups, the LSP (`ts_ls`) will work just fine
-        -- ts_ls = {},
+        vtsls = {
+          settings = {
+            vtsls = {
+              autoUseWorkspaceTsdk = true,
+              enableMoveToFileCodeAction = true,
+            },
+            typescript = {
+              implementationsCodeLens = { enabled = false },
+              referencesCodeLens = { enabled = false },
+              updateImportsOnFileMove = { enabled = 'always' },
+            },
+            javascript = {
+              referencesCodeLens = { enabled = false },
+              updateImportsOnFileMove = { enabled = 'always' },
+            },
+          },
+        },
         ansiblels = {},
         lua_ls = {
           -- cmd = {...},
