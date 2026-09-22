c38de8a80a1c0ab3105ff959e8411a28336ce3fc
xamfoo <xamfoo@icloud.com>
2026-09-22 11:24:35 +0800

    Add peek.nvim for markdown preview

--- init.lua
+++ init.lua
@@ -927,6 +927,19 @@ require('lazy').setup({
       })
     end,
   },
+  {
+    'toppair/peek.nvim',
+    event = { 'VeryLazy' },
+    build = 'deno task --quiet build:fast',
+    cond = function()
+      return vim.fn.executable 'deno' == 1
+    end,
+    config = function()
+      require('peek').setup()
+      vim.api.nvim_create_user_command('PeekOpen', require('peek').open, {})
+      vim.api.nvim_create_user_command('PeekClose', require('peek').close, {})
+    end,
+  },
   { -- Bindings for (un)commenting
     'tpope/vim-commentary',
     version = false,
